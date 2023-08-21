fdc30c84a42685fd1d2d06fc6b96991172a9fddf
shin <990114js@naver.com>
2023-08-21 15:57:50 +0900

    lambda

--- chapter_05_01.py
+++ chapter_05_01.py
@@ -74,3 +74,74 @@ def func_mul2(x):
 d = func_mul2(10)
 
 print(type(d), d, d.items(), d.keys())
+
+
+# 중요
+# *args, **kwargs
+
+
+# *args 언팩킹
+# *이 없으면 하나하나의 요소로 생각
+# *이 있으면 그 자체를 하나의 index로 생각
+def args_func(*args):  # 매개변수 명 자유
+    for i, v in enumerate(args):
+        print("result: {}".format(i), v)
+    print("----")
+
+
+args_func("lee")
+args_func("lee", "park")
+
+
+# **kwargs 언팩킹
+def kwargs_func(**kwargs):  # 매개변수 명 자유
+    for v in kwargs.keys():
+        print("{}".format(v), kwargs[v])
+    print("----")
+
+
+kwargs_func(name1="hello")
+kwargs_func(name1="hello", name2="mello")
+
+
+# 전체 혼합
+def example(args_1, args_2, *args, **kwargs):
+    print(args_1, args_2, args, kwargs)
+
+
+example(10, 20, "lee", "kim", "park", "20", age1=10, age2=20, age3=50)
+
+
+# 중첩함수
+def nested_func(num):
+    def func_in_func(num):  # 함수 설정
+        print(num)
+
+    print("in func")
+    func_in_func(num + 100)  # 함수 안의 함수 실행
+
+
+nested_func(100)
+
+# 람다식 예제
+# 메모리 절약, 가독성 향상, 코드 간결
+# 함수: 객체 생성 -> 메모리 할당
+# 람다식: 즉시 실행 함수(heap 초기화) -> 메모리 초기화
+# 남발시 가독성 감소
+
+
+def mul_func(x, y):
+    return x * y
+
+
+a = mul_func(10, 50)
+print(a)
+lambda_mul_func = lambda x, y: x * y
+print(lambda_mul_func(50, 30))
+
+
+def func_final(x, y, func):
+    print(x * y * func(100, 100))
+
+
+func_final(10, 20, lambda x, y: x * y)
